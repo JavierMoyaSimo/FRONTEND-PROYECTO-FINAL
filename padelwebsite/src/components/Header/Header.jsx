20e855d65217714999ccf85ed555ccdc41ad2d01
Javi <javiermoyasimo@gmail.com>
2023-01-04 16:58:18 +0100

    feat: Header completed. Redux added.Added rendering depending on whether you have token or not

--- padelwebsite/src/components/Header/Header.jsx
+++ padelwebsite/src/components/Header/Header.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import './Header.scss';
-
+import { useSelector } from "react-redux";
+import { userData } from "../../containers/User/userSlice";
+import './Header.scss'
 import { useNavigate } from 'react-router-dom';
+
+
+
+
+
 
 const Header = () => {
 
+
     const navigate = useNavigate();
+    const userReduxCredentials = useSelector(userData);
 
-    return (
-        <div className="headerDesign">
-            <div onClick={() => navigate("/login")} className='linkDesign'>Login</div>
-            <div onClick={() => navigate("/register")} className='linkDesign'>Register</div>
-        </div>
-    )
+
+
+
+    if (userReduxCredentials?.credentials?.jwt !== undefined) {
+
+
+        return (
+            <div className='headerDesign'>
+                <div onClick={() => navigate("/")} className="linkDesign">Home</div>
+                {/* <div className='titleDesign1' onClick={() => navigate('/')}>Pop-up Films</div> */}
+                <div onClick={() => navigate("/profile")} className="linkDesign">{userReduxCredentials?.credentials?.name}</div>
+            </div>
+        )
+    } else {
+
+
+        return (
+            <div className='headerDesign'>
+
+                <div onClick={() => navigate('/login')} className="linkDesign">Login</div>
+                <div className='titleDesign1' onClick={() => navigate('/')}>Home</div>
+                <div onClick={() => navigate('/register')} className="linkDesign">Register</div>
+            </div>
+        )
+    }
+
+
 }
-
 export default Header;
