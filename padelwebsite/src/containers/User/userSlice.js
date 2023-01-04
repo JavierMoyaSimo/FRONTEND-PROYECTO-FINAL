f9ade254e263829d20039221bb48306a70f011d8
Javi <javiermoyasimo@gmail.com>
2023-01-04 16:26:59 +0100

    feat: create userSlice. Create initialState and login in reducers

---
+++ padelwebsite/src/containers/User/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from 'reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        token: ''
+    },
+    reducers: {
+        login: (state, action) => {
+            return {
+                ...action.payload
+            }
+        }
+    }
+});
+
+export const { login } = userSlice.actions;
+
+export const userData = (state) => state.user;
+
+export default userSlice.reducer;
