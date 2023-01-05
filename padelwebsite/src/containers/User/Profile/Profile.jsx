e042da7449bdc55ea32202e735c69c5dc715c44e
Javi <javiermoyasimo@gmail.com>
2023-01-05 12:10:27 +0100

    feat: Profile created. Profile scss created. Logout, Reserved games and user Settings added in Profile.jsx

---
+++ padelwebsite/src/containers/User/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Profile.scss";
+import { userData, userout } from "../userSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+const Profile = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userReduxCredentials = useSelector(userData);
+
+    const logout = () => {
+        if (userReduxCredentials?.credentials?.jwt !== undefined) {
+
+            dispatch(userout({ credentials: {} }));
+
+
+            return navigate("/");
+        }
+    };
+
+    return (
+        <div className="menuDesign">
+            <div className="menuButtonDesign" onClick={() => navigate("/userBooking")}>
+                Reserved games
+            </div>
+            <div className="menuButtonDesign" onClick={() => navigate("/userSettings")}>
+                User Settings
+            </div>
+            <div className="menuButtonDesign logOutDesign" onClick={logout}>
+                Log Out
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
